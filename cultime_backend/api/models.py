bb4e5684bd142c67bf868830baf071889fec872b
Andrej Velichkovski <andrej.velichkovski@gmail.com>
2021-02-16 15:20:16 +0100

    Added new tables in the database

--- cultime_backend/api/models.py
+++ cultime_backend/api/models.py
@@ -4,8 +4,32 @@ from django.core.exceptions import NON_FIELD_ERRORS
 
 # Table for following system
 class Follow(models.Model):
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
 
     def __str__(self):
         return str(self.following.username) + " follows " + str(self.follower.username)
+
+
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    movie_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.movie_name) + "has id: " + str(self.movie_id)
+
+
+class MovieWatched(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie')
+
+
+class WatchList(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
