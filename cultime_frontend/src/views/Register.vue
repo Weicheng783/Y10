d13c00e246e195c3c0cde16a69856a55b49f9f68
d73761aa <awais.ahmed-3@student.manchester.sch.uk>
2021-03-12 15:38:35 +0000

    Register page nearly complete

--- cultime_frontend/src/views/Register.vue
+++ cultime_frontend/src/views/Register.vue
@@ -6,44 +6,59 @@
       <router-link to="/register">Create Account</router-link>
     </div>
 
-    <h4>Register</h4>
-      <form>
-        <label for="first_name">First Name</label>
-        <div>
-          <input id="first_name" type="text" v-model="first_name" required autofocus>
+      <form class="form">
+
+        <h3 style="color:white;  margin:5px;">───────Create Account───────</h3>
+
+        <div class="field">
+         <label for="first_name">First Name</label>
+         <div class="control">
+           <input style="width: 100%" placeholder="First Name" id="first_name" type="text" v-model="first_name" required autofocus>
+         </div>
         </div>
 
-        <label for="last_name">Last Name</label>
-        <div>
-          <input id="last_name" type="text" v-model="last_name" required autofocus>
+        <div class="field">
+          <label for="last_name">Last Name</label>
+          <div>
+            <input style="width: 100%" placeholder="Last Name" id="last_name" type="text" v-model="last_name" required autofocus>
+          </div>
         </div>
 
-        <label for="username">Username</label>
-        <div>
-          <input id="username" type="text" v-model="username" required autofocus>
+        <div class="field">
+          <label for="username">Username</label>
+          <div>
+            <input style="width: 100%" placeholder="Username" id="username" type="text" v-model="username" required autofocus>
+          </div>
         </div>
 
-        <label for="email" >E-Mail Address</label>
-        <div>
-          <input id="email" type="email" v-model="email" required>
+        <div class="field">
+          <label for="email" >E-Mail Address</label>
+          <div>
+            <input style="width: 100%" placeholder="E-Mail" id="email" type="email" v-model="email" required>
+          </div>
         </div>
 
-        <label for="password">Password</label>
-        <div>
-          <input id="password" type="password" v-model="password" required>
+        <div class="field">
+          <label for="password">Password</label>
+          <div>
+            <input style="width: 100%" placeholder="Password" id="password" type="password" v-model="password" required>
+          </div>
         </div>
 
-        <label for="password-confirm">Confirm Password</label>
-        <div>
-          <input id="password-confirm" type="password" v-model="password_confirmation" required>
+        <div class="field">
+          <label for="password-confirm">Confirm Password</label>
+          <div>
+            <input style="width: 100%" placeholder="Password" id="password-confirm" type="password" v-model="password_confirmation" required>
+          </div>
         </div>
 
         <div>
-          <button type="submit" @click="handleSubmit">
+          <button style="margin-top: 20px;" type="submit" @click="handleSubmit">
             Register
           </button>
         </div>
       </form>
+
   </div>
 </template>
 
@@ -72,7 +87,7 @@ export default {
       e.preventDefault()
       if (this.password === this.password_confirmation && this.password.length > 0)
       {
-        let url = "http://localhost:8000/register/"
+        let url = "http://localhost:8000/authentication/register/"
         axios.post(url, {
           username: this.username,
           password: this.password,
@@ -111,3 +126,54 @@ export default {
   },
 }
 </script>
+
+<style>
+    
+    input{
+      border: #black;
+      border-radius: 8px;
+      background-color:#262728;
+      text-align:center;
+      padding:15px 145px;
+      
+    }
+
+    label{
+      float: left;
+      padding-bottom: 5px;
+      color:white;
+      
+      font-weight: bold;
+    }
+
+    button{
+      font-weight:bold;
+      color: white;
+      background-color:#036FC0;
+      border:#036FC0;
+      padding: 15px 150px;
+      border-radius: 8px;
+      cursor: pointer;
+      font-size: 20px;
+      
+
+    }
+
+    button:hover{
+      background-color: #065591;
+    }
+
+    
+
+
+    .form{
+      float: left;
+      border: 5px solid #262728;
+      border-radius: 20px;
+      background-color: #262728;
+      margin-left: 200px;
+      padding: 20px 20px 20px 20px;
+      
+
+    }
+</style>
