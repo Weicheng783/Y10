5311970eb6898087259cc6f561ce0a0fddf2dc88
d73761aa <awais.ahmed-3@student.manchester.sch.uk>
2021-03-13 20:46:27 +0000

    Completed Register Page

--- cultime_frontend/src/views/Register.vue
+++ cultime_frontend/src/views/Register.vue
@@ -5,50 +5,50 @@
       <router-link to="/login">Login</router-link> |
       <router-link to="/register">Create Account</router-link>
     </div>
+    
+    <img style="width:600px; position:absolute; height:750px; margin-left: 300px; margin-top: -25px; padding: 25px 20px;border-radius:40px;" src="https://i.ibb.co/wJMG8Mx/Screenshot-2021-03-01-at-10-00-45.png" 
+         alt="Screenshot-2021-03-01-at-10-00-45" width="550">    
 
       <form class="form">
 
-        <h3 style="color:white;  margin:5px;">───────Create Account───────</h3>
+        <h3 style="color:white;"><b>───────Create Account───────</b></h3>
 
-        <div class="field">
-         <label for="first_name">First Name</label>
-         <div class="control">
-           <input style="width: 100%" placeholder="First Name" id="first_name" type="text" v-model="first_name" required autofocus>
+        <div class="field" style="margin-top: 20px;">
+         <label for="first_name" style="font-size: 12px; margin-top: 15px; margin-right: 13px;">First Name</label>
+         <div>
+           <input style="text-align:center; float:left;" size="12" placeholder="First Name"  id="first_name" type="text" v-model="first_name" required autofocus>
          </div>
-        </div>
-
-        <div class="field">
-          <label for="last_name">Last Name</label>
+         <label for="last_name" style="font-size: 12px; margin-left: 13px; margin-top: 15px; margin-right: 13px;">Last Name</label>
           <div>
-            <input style="width: 100%" placeholder="Last Name" id="last_name" type="text" v-model="last_name" required autofocus>
+            <input style="text-align: center;" size="12"  placeholder="Last Name" id="last_name" type="text" v-model="last_name" required autofocus>
           </div>
         </div>
 
         <div class="field">
           <label for="username">Username</label>
           <div>
-            <input style="width: 100%" placeholder="Username" id="username" type="text" v-model="username" required autofocus>
+            <input style="text-align: center" size="52"  placeholder="Username" id="username" type="text" v-model="username" required autofocus>
           </div>
         </div>
 
         <div class="field">
           <label for="email" >E-Mail Address</label>
           <div>
-            <input style="width: 100%" placeholder="E-Mail" id="email" type="email" v-model="email" required>
+            <input style="text-align: center" size="52"  placeholder="E-Mail" id="email" type="email" v-model="email" required>
           </div>
         </div>
 
         <div class="field">
           <label for="password">Password</label>
           <div>
-            <input style="width: 100%" placeholder="Password" id="password" type="password" v-model="password" required>
+            <input style="text-align: center" size="52"  placeholder="Password" id="password" type="password" v-model="password" required>
           </div>
         </div>
 
         <div class="field">
           <label for="password-confirm">Confirm Password</label>
           <div>
-            <input style="width: 100%" placeholder="Password" id="password-confirm" type="password" v-model="password_confirmation" required>
+            <input style="text-align: center" size="52"  placeholder="Password" id="password-confirm" type="password" v-model="password_confirmation" required>
           </div>
         </div>
 
@@ -133,8 +133,7 @@ export default {
       border: #black;
       border-radius: 8px;
       background-color:#262728;
-      text-align:center;
-      padding:15px 145px;
+      height: 50px;
       
     }
 
@@ -172,6 +171,7 @@ export default {
       border-radius: 20px;
       background-color: #262728;
       margin-left: 200px;
+      margin-top: 40px;
       padding: 20px 20px 20px 20px;
       
 
